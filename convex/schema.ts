8421dc935964f860283dc6ade4cd3c0586c356ee
Emma Forman Ling <emma@convex.dev>
2023-09-20 11:37:31 -0700

    schema for recipes that were imported

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,6 +3,15 @@ import { authTables } from "@convex-dev/convex-lucia-auth";
 import { v } from "convex/values";
 
 export default defineSchema({
+  recipes: defineTable({
+    NER: v.string(),
+    directions: v.string(),
+    id: v.float64(),
+    ingredients: v.string(),
+    link: v.string(),
+    source: v.string(),
+    title: v.string(),
+  }),
   ...authTables({
     user: {
       email: v.string(),
